cef5b5a43121656fc2d63fc8fe0a4fdf13bbca93
tomohiro-kushida <70782980+tmhr0@users.noreply.github.com>
2024-09-05 02:07:52 +0000

    fix: 非ログイン状態の時のみ、ログインボタンを表示される

--- js/check-login.js
+++ js/check-login.js
@@ -1,7 +1,17 @@
 import { supabase } from './supabase.js';
 
-document.addEventListener('DOMContentLoaded', () => {
- const linksToCheck = [
+document.addEventListener('DOMContentLoaded', async () => {
+  const loginButton = document.querySelector('.blue-btn');
+
+  // ログイン状態を確認
+  const { data: { session }, error } = await supabase.auth.getSession();
+
+  if (error || !session) {
+      // 非ログイン状態の場合、ログインボタンを表示
+      loginButton.classList.remove('btn-hidden');
+  }
+
+  const linksToCheck = [
   { id: 'vote-link', href: '/vote' },
   { id: 'confirm-vote-btn', href: '/vote' },
  ];
